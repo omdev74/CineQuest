57f51ba2ffb6c50b66e1d5ec3a11e0864ceb51c3
Om dev <43951418+omdev74@users.noreply.github.com>
2023-12-12 14:10:33 -0500

    Dashboar user details fetching in the card

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -14,27 +14,27 @@ import MovieList from "../MovieList/MovieList";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
 
-// Dashboard Component
 const Dashboard = () => {
   const navigate = useNavigate();
 
-  // Sample data
-  const [data, setData] = useState(null); // Initialize the data state
+  // playlist
+  const [data, setData] = useState(null);
 
   const apiKey = "9ac88c47d4d586add1154d12a91509f7";
   const tmdbEndpoint = "https://api.themoviedb.org/3/trending/movie/week";
+  
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    // Check if the user is logged in
     const token = getCookie("userToken");
 
     if (!token) {
-      // If the user is not logged in, redirect to the login page
       navigate("/login");
     }
 
-    // Fetch the data inside the useEffect hook if the user is authenticated
-    console.log("useEffect");
+    // Fetch user profile data from the API
+    fetchUserProfile(token);
+
     fetch(`${tmdbEndpoint}?api_key=${apiKey}`)
       .then((response) => {
         if (!response.ok) {
@@ -43,17 +43,36 @@ const Dashboard = () => {
         return response.json();
       })
       .then((fetchedData) => {
-        // Update the data state with the fetched data
-        console.log(fetchedData);
         setData(fetchedData);
       })
       .catch((error) => {
-        // Handle any errors here
         console.error("Error:", error);
       });
-  }, [navigate]); // Add navigate to the dependency array
+  }, [navigate]);
 
-  // Function to get a cookie by name
+
+  const fetchUserProfile = (token) => {
+    fetch("https://moviesearch-api.onrender.com/user/profile", {
+      method: "GET",
+      headers: {
+        Authorization: `${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((userData) => {
+        // Update the state with user data
+        setUserData(userData);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   const getCookie = (name) => {
     const value = "; " + document.cookie;
     const parts = value.split("; " + name + "=");
@@ -66,55 +85,35 @@ const Dashboard = () => {
         Movie App Dashboard
       </Typography>
       <Typography variant="body1">
-        Welcome to your movie app dashboard. Here, you can manage your
-        playlists, change your password, edit your profile, and update your
-        profile photo.
+        Welcome to your movie app dashboard. Here, you can manage your playlists,
+        change your password, edit your profile, and update your profile photo.
       </Typography>
 
-      {/* Section: My Playlists */}
-      <Paper elevation={3} mt={3} sx={{ backgroundColor: "#100f12" }}>
+      <Paper elevation={3} mt={3} className="dashboard-section">
         <Box p={2}>
           <Typography variant="h6">My Playlists</Typography>
           <MovieList movies={data} numberOfMovies={5} excludeFirst />
         </Box>
       </Paper>
-      <Divider
-        style={{
-          color: "white",
-          border: "1px solid #444444",
-        }}
-      />
-      {/* Section: Change Password */}
-      <Paper elevation={3} mt={3} sx={{ backgroundColor: "#100f12" }}>
-        <Box p={2} className="DashboardForm">
+
+      <Divider className="dashboard-divider" />
+
+      <Paper elevation={3} mt={3} className="dashboard-section">
+        <Box p={2} className="dashboard-form">
           <Typography variant="h6">Change Password</Typography>
-          <TextField
-            label="Current Password"
-            type="password"
-            fullWidth
-            mt={2}
-          />
+          <TextField label="Current Password" type="password" fullWidth mt={2} />
           <TextField label="New Password" type="password" fullWidth mt={2} />
-          <TextField
-            label="Confirm New Password"
-            type="password"
-            fullWidth
-            mt={2}
-          />
+          <TextField label="Confirm New Password" type="password" fullWidth mt={2} />
           <Button variant="contained" color="primary" fullWidth mt={2}>
             Change Password
           </Button>
         </Box>
       </Paper>
-      <Divider
-        style={{
-          color: "white",
-          border: "1px solid #444444",
-        }}
-      />
-      {/* Section: Edit Profile */}
-      <Paper elevation={3} mt={3} sx={{ backgroundColor: "#100f12" }}>
-        <Box p={2} className="DashboardForm">
+
+      <Divider className="dashboard-divider" />
+
+      <Paper elevation={3} mt={3} className="dashboard-section">
+        <Box p={2} className="dashboard-form">
           <Typography variant="h6">Edit Profile</Typography>
           <TextField label="Full Name" fullWidth mt={2} />
           <TextField label="Email" fullWidth mt={2} />
@@ -124,32 +123,26 @@ const Dashboard = () => {
           </Button>
         </Box>
       </Paper>
-      <Divider
-        style={{
-          color: "white",
-          border: "1px solid #444444",
-        }}
-      />
-      {/* Section: User Data */}
-      <Paper elevation={3} mt={3} sx={{ backgroundColor: "#100f12" }}>
-        <Box p={2} className="DashboardForm">
-          <Typography variant="h6">User Data</Typography>
-          <Avatar
-            alt="Your Profile Photo"
-            src="./images/your-profile-photo.jpg"
-            sx={{ width: 150, height: 150, mt: 2 }}
-          />
-          <Typography variant="body1">
-            Full Name: {sessionStorage.getItem("fullName") || "N/A"}
-          </Typography>
-          <Typography variant="body1">
-            Email: {sessionStorage.getItem("email") || "N/A"}
-          </Typography>
-          <Typography variant="body1">
-            Username: {sessionStorage.getItem("username") || "N/A"}
-          </Typography>
-        </Box>
-      </Paper>
+
+      <Divider className="dashboard-divider" />
+
+      <Paper elevation={3} mt={3} className="dashboard-section">
+  <Box p={2} className="dashboard-form">
+    <Typography variant="h6">User Data</Typography>
+    {userData && (
+      <>
+        <Avatar
+          alt="Your Profile Photo"
+          src={userData.profilePhoto || "./images/default-profile-photo.jpg"}
+          sx={{ width: 150, height: 150, mt: 2 }}
+        />
+        <Typography variant="body1" sx={{ color: 'white' }}>Username: {userData.data.username || "N/A"}</Typography>
+        <Typography variant="body1" sx={{ color: 'white' }}>Email: {userData.data.email || "N/A"}</Typography>
+        <Typography variant="body1" sx={{ color: 'white' }}>Member since: {userData.data.createdAt || "N/A"}</Typography>
+      </>
+    )}
+  </Box>
+</Paper>
     </Container>
   );
 };
